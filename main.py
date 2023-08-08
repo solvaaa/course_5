d4e3474eb040ea788fc6b6baa54e6a6f9a8082c5
Alexandra <alexandrasolovjova@mail.ru>
2023-08-08 20:48:15 +0300

    modify main

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from src.dbmanager import DBManager
-from src.database_fill import fill_employer_table, fill_vacancies_table
+from src.database_fill import create_database, fill_employer_table, fill_vacancies_table
 
 
 def interaction():
     dbmanager = DBManager()
-#    dbmanager.create_database()
-#    fill_employer_table()
-#    fill_vacancies_table()
+    create_database()
+    fill_employer_table()
+    fill_vacancies_table()
     result = dbmanager.get_all_vacancies()
     print(*result, sep='\n')
 
