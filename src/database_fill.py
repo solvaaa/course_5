1d69e9cd60dd733cfd6eef2041f0a75b149f0121
Alexandra <alexandrasolovjova@mail.ru>
2023-08-08 20:48:09 +0300

    move dbmanager create_database to database_fill module

--- src/database_fill.py
+++ src/database_fill.py
@@ -1,9 +1,33 @@
 import psycopg2
 from src.api import HeadHunter
 from src.config import config
-
-
+CREATE_TABLES_PATH = 'src/create_tables.sql'
+QUERIES_PATH = 'src/queries.sql'
 EMPLOYERS = [1740, 3529, 80, 3776, 3127, 1122462, 9352463, 2748, 6836, 230005]
+
+
+def create_database(create_tables_path=CREATE_TABLES_PATH, params=config()):
+    dbname = params['database']
+    del params['database']
+    conn = psycopg2.connect(dbname='postgres', **params)
+    conn.autocommit = True
+    cur = conn.cursor()
+    try:
+        cur.execute(f"DROP DATABASE {dbname}")
+    except psycopg2.errors.InvalidCatalogName:
+        pass
+    cur.execute(f"CREATE DATABASE {dbname}")
+    conn.close()
+
+    conn = psycopg2.connect(dbname=dbname, **params)
+
+    with open(create_tables_path, 'r', encoding='utf-8') as queries_file:
+        query = queries_file.read()
+
+    with conn.cursor() as cur:
+        cur.execute(query)
+    conn.commit()
+    conn.close()
 
 
 def fill_employer_table(employers=EMPLOYERS):
--- src/dbmanager.py
+++ src/dbmanager.py
@@ -1,40 +1,14 @@
 from src.config import config
 from src.parser import QueryParser
 import psycopg2
-CREATE_TABLES_PATH = 'src/create_tables.sql'
-QUERIES_PATH = 'src/queries.sql'
+
 
 
 class DBManager:
 
-    def __init__(self, create_tables_path=CREATE_TABLES_PATH,
-                 queries_path=QUERIES_PATH):
-        self.__create_tables_path = create_tables_path
+    def __init__(self):
         self.__parser = QueryParser()
-        self.__parser.read(queries_path)
-
-    def create_database(self, params=config()):
-        dbname = params['database']
-        del params['database']
-        conn = psycopg2.connect(dbname='postgres', **params)
-        conn.autocommit = True
-        cur = conn.cursor()
-        try:
-            cur.execute(f"DROP DATABASE {dbname}")
-        except psycopg2.errors.InvalidCatalogName:
-            pass
-        cur.execute(f"CREATE DATABASE {dbname}")
-        conn.close()
-
-        conn = psycopg2.connect(dbname=dbname, **params)
-
-        with open(self.__create_tables_path, 'r', encoding='utf-8') as queries_file:
-            query = queries_file.read()
-
-        with conn.cursor() as cur:
-            cur.execute(query)
-        conn.commit()
-        conn.close()
+        self.__parser.read()
 
     def execute_query(self, query_name, query_params={}, params=config()):
         conn = psycopg2.connect(**params)
