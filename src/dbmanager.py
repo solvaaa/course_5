009d04c8cf1617daa4181936bfe3d73efb2ef814
Alexandra <alexandrasolovjova@mail.ru>
2023-08-08 15:39:45 +0300

    add create database method

--- src/dbmanager.py
+++ src/dbmanager.py
@@ -1,4 +1,5 @@
 from api import HeadHunter
+from config import config
 import psycopg2
 
 
@@ -6,3 +7,25 @@ class DBManager:
 
     def __init__(self):
         pass
+
+    def create_database(self, params):
+        dbname = params['database']
+        del params['database']
+        conn = psycopg2.connect(dbname='postgres', **params)
+        conn.autocommit = True
+        cur = conn.cursor()
+        try:
+            cur.execute(f"DROP DATABASE {dbname}")
+            print('dropped')
+        except psycopg2.errors.InvalidCatalogName:
+            pass
+        cur.execute(f"CREATE DATABASE {dbname}")
+        conn.close()
+
+        conn = psycopg2.connect(dbname=dbname, **params)
+
+
+man = DBManager()
+params = config()
+print(config())
+man.create_database(params)
