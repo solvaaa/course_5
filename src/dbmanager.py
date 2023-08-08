cc1597c22b3fe9ffb1e7e18e958131dc5656e86c
Alexandra <alexandrasolovjova@mail.ru>
2023-08-08 17:47:27 +0300

    add execute_query method

--- src/dbmanager.py
+++ src/dbmanager.py
@@ -2,13 +2,18 @@ from config import config
 from parser import QueryParser
 import psycopg2
 CREATE_TABLES_PATH = 'create_tables.sql'
+QUERIES_PATH = 'queries.sql'
+
 
 class DBManager:
 
-    def __init__(self):
-        pass
+    def __init__(self, create_tables_path=CREATE_TABLES_PATH,
+                 queries_path=QUERIES_PATH):
+        self.create_tables_path = create_tables_path
+        self.parser = QueryParser()
+        self.parser.read(queries_path)
 
-    def create_database(self, params):
+    def create_database(self, params=config()):
         dbname = params['database']
         del params['database']
         conn = psycopg2.connect(dbname='postgres', **params)
@@ -23,10 +28,23 @@ class DBManager:
 
         conn = psycopg2.connect(dbname=dbname, **params)
 
-        with open(CREATE_TABLES_PATH, 'r', encoding='utf-8') as queries_file:
+        with open(self.create_tables_path, 'r', encoding='utf-8') as queries_file:
             query = queries_file.read()
 
         with conn.cursor() as cur:
             cur.execute(query)
         conn.commit()
         conn.close()
+
+    def execute_query(self, query_name, params=config()):
+        conn = psycopg2.connect(**params)
+        query = self.parser.get_item(query_name)
+        with conn.cursor() as cur:
+            cur.execute(query)
+        conn.commit()
+        conn.close()
+
+
+#man = DBManager()
+#man.create_database(config())
+#man.execute_query('add_employer')
